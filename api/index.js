c99b03da078fec1ee9fab7540b84781b0d796f5f
JadiDapa <daffafalthaf25@gmail.com>
2024-05-16 17:47:28 +0700

    feat : vercel config

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+const AccountRouter = require('../src/routes/route.account');
+const MaterialRouter = require('../src/routes/route.material');
+const MaterialCategoryRouter = require('../src/routes/route.material-category');
+const MaterialInventoryRouter = require('../src/routes/route.material-inventory');
+const MaterialUpdatesRouter = require('../src/routes/route.material-updates');
+const ToolRouter = require('../src/routes/route.tool');
+const ToolCategoryRouter = require('../src/routes/route.tool-category');
+const ToolInventoryRouter = require('../src/routes/route.tool-inventory');
+const ToolUpdatesRouter = require('../src/routes/route.tool-updates');
+const VehicleRouter = require('../src/routes/route.vehicle');
+const VehicleVariantRouter = require('../src/routes/route.vehicle-variant');
+const AccountUnitRouter = require('../src/routes/route.account-unit');
+const RequestRouter = require('../src/routes/route.request');
+const RequestItemRouter = require('../src/routes/route-request-item');
+
+app.get('/', (req, res) => res.send('Express on Vercel'));
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', AccountRouter);
+app.use('/api', AccountUnitRouter);
+app.use('/api', MaterialRouter);
+app.use('/api', MaterialCategoryRouter);
+app.use('/api', MaterialInventoryRouter);
+app.use('/api', MaterialUpdatesRouter);
+app.use('/api', ToolRouter);
+app.use('/api', ToolCategoryRouter);
+app.use('/api', ToolInventoryRouter);
+app.use('/api', ToolUpdatesRouter);
+app.use('/api', VehicleRouter);
+app.use('/api', VehicleVariantRouter);
+app.use('/api', RequestRouter);
+app.use('/api', RequestItemRouter);
+
+app.listen(port, () => {
+  console.log('Server aktif');
+  console.log(`http://localhost:${port}`);
+});
--- src/routes/route.account-unit.js
+++ src/routes/route.account-unit.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const AccountUnitRouter = express.Router();
 const AccountUnitController = require('../controllers/controller.account-unit');
+const { authCheck, adminAuthCheck } = require('../helpers/helper.authorization');
 
 AccountUnitRouter.get('/account-units', AccountUnitController.getAccountUnits);
 AccountUnitRouter.get('/account-units/:accountUnitId', AccountUnitController.getAccountUnitById);
-AccountUnitRouter.post('/account-units/create', AccountUnitController.createAccountUnit);
+AccountUnitRouter.post(
+  '/account-units/create',
+  authCheck,
+  adminAuthCheck,
+  AccountUnitController.createAccountUnit
+);
 
 module.exports = AccountUnitRouter;
